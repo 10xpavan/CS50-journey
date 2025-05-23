a8a974ebd7f12607d2e195f1fb9573e4b882db55
10xpavan <pavan.nulenur@gmail.com>
2025-05-23 19:35:48 +0530

    for loop and while loop

---
+++ Lecture 1-C/cat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "cs50.h"
+void meow(void);
+int main(void)
+{
+    int i = 0;
+    while (i < 3)
+    {
+        printf("meow\n");
+        i++;
+    }
+
+    for (int i = 0; i < 3; i ++)
+    {
+        meow();
+    }
+
+}
+
+void meow(void)
+{
+    printf("meow\n");
+}
